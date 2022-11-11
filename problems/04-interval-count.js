aa24d068d2359a5f5472836f396e67f99f041089
Leo Ọládimú <82811737+leooladimu@users.noreply.github.com>
2022-11-10 19:16:25 -0500

    3

--- problems/04-interval-count.js
+++ problems/04-interval-count.js
@@ -17,9 +17,35 @@ intervalCount(function() {
 ***********************************************************************/
 
 function intervalCount(cb, delay, amount) {
-  // Your code here
+  // let count = 0;
+  // var interval = setInterval(cb, delay)
+  // for (let i = 1; i <= amount; i++) {
+    // return function() {
+      // amount--
+
+      let interval = setInterval(() => {
+        if (amount === 0) {
+          clearInterval(interval)
+        }
+        cb()
+        amount--
+      }, delay)
+      // console.log(interval);
+      // clearInterval(interval)
+      // console.log(clearInterval(interval));
+
+    // }
+ 
+  // }
+  // clearInterval(interval)
+  // console.log(clearInterval(interval));
+
 }
 
+
+// intervalCount(function() {
+//   console.log('hi');
+// }, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = intervalCount;
